8d43371cb41cdd00be63203c3ced805d82b414c3
rox-streuli <roxstreuli@gmail.com>
2023-12-26 19:26:08 +0000

    Refactor turtles creation.

--- venv/main.py
+++ venv/main.py
@@ -45,24 +45,19 @@ def user_won(player, user_bet):
         return True
 
 
+colours = ['cyan', 'pink', 'green', 'red', 'blue', 'yellow', 'orange']
+positions = [0, -100, -200, -300, 100, 200, 300]
+turtles_in_race = []
 # Creating 6 instances of Turtle
-def create_turtle(colour, x_pos, y_pos):
+
+for indx, colour in enumerate(colours):
     player = Turtle(shape='turtle')
-    player.speed('normal')
-    player.shapesize(3)
     player.color(colour)
+    player.shapesize(3)
+    player.speed('fast')
     player.penup()
-    player.setposition(x_pos, y_pos)
-    return player.fillcolor()
-
-
-create_turtle('red', -450, 0)
-create_turtle('blue', -450, -100)
-create_turtle('yellow', -450, -200)
-create_turtle('orange', -450, -300)
-create_turtle('green', -450, 100)
-create_turtle('violet', -450, 200)
-create_turtle('cyan', -450, 300)
+    player.setposition(x=-450, y=positions[indx])
+    turtles_in_race.append(player)
 
 # store user's bet
 bet = turtle.textinput("Make your bet", "Guess wich turtle will win the race: ")
