a8614ca2b8517c71bbcd9f670e2c99bf68fc68d9
rox-streuli <roxstreuli@gmail.com>
2023-12-28 14:07:38 +0000

    Create control variable play to reset game and play again.

--- venv/main.py
+++ venv/main.py
@@ -1,81 +1,87 @@
 import turtle
-from turtle import Turtle, Screen
 from random import randint
+from turtle import Turtle, Screen
 
 # create screen object and set colour bkacground
 screen = Screen()
-screen.bgcolor('darkseagreen')
 
-# Write a title
-banner = Turtle()
-banner.hideturtle()
-banner.penup()
-banner.goto(0, 400)
-banner.color('darkred')
-banner.write("Welcome Turtle Race", align='center',  font=('verdana', 25,
-                                                           'normal'))
-# write turte colours
-runners = Turtle()
-runners.hideturtle()
-runners.penup()
-runners.goto(-300, -400)
-runners.color('black')
-runners.write("Turtles: cyan, pink, green, red, blue, yellow, orange",
-              align='left',  font=('verdana', 13, 'normal'))
+play = True
+if play:
+    screen.bgcolor('darkseagreen')
 
-# Draw finish line
-finish_line = Turtle()
-finish_line.pensize(3)
-finish_line.hideturtle()
-finish_line.penup()
-finish_line.speed('fastest')
-finish_line.setposition(400, -400)
-finish_line.left(90)
-finish_line.pendown()
-finish_line.forward(800)
-f_line_pos = 400
+    # Write a title
+    banner = Turtle()
+    banner.hideturtle()
+    banner.penup()
+    banner.goto(0, 400)
+    banner.color('darkred')
+    banner.write("Welcome Turtle Race", align='center', font=('verdana', 25,
+                                                              'normal'))
+    # write turte colours
+    runners = Turtle()
+    runners.hideturtle()
+    runners.penup()
+    runners.goto(-300, -400)
+    runners.color('black')
+    runners.write("Turtles: cyan, pink, green, red, blue, yellow, orange",
+                  align='left', font=('verdana', 13, 'normal'))
 
-# Turtles attributes
-colours = ['cyan', 'pink', 'green', 'red', 'blue', 'yellow', 'orange']
-positions = [0, -100, -200, -300, 100, 200, 300]
+    # Draw finish line
+    finish_line = Turtle()
+    finish_line.pensize(3)
+    finish_line.hideturtle()
+    finish_line.penup()
+    finish_line.speed('fastest')
+    finish_line.setposition(400, -400)
+    finish_line.left(90)
+    finish_line.pendown()
+    finish_line.forward(800)
+    f_line_pos = 400
 
-turtles_in_race = []
-# Creating 6 instances of Turtle
+    # Turtles attributes
+    colours = ['cyan', 'pink', 'green', 'red', 'blue', 'yellow', 'orange']
+    positions = [0, -100, -200, -300, 100, 200, 300]
 
-for indx, colour in enumerate(colours):
-    player = Turtle(shape='turtle')
-    player.color(colour)
-    player.shapesize(3)
-    player.speed('normal')
-    player.penup()
-    player.setposition(x=-450, y=positions[indx])
-    turtles_in_race.append(player)
+    turtles_in_race = []
+    # Creating 6 instances of Turtle
 
-# store user's bet
-bet = ''
-while bet not in ['cyan', 'pink', 'green', 'red', 'blue', 'yellow', 'orange']:
-    bet = turtle.textinput("Make your bet", "Guess wich turtle will win the race: ")
+    for indx, colour in enumerate(colours):
+        player = Turtle(shape='turtle')
+        player.color(colour)
+        player.shapesize(3)
+        player.speed('normal')
+        player.penup()
+        player.setposition(x=-450, y=positions[indx])
+        turtles_in_race.append(player)
 
-start_race = False
-winner = ""
-if bet:
-    start_race = True
+    # store user's bet
+    bet = ''
+    while bet not in ['cyan', 'pink', 'green', 'red', 'blue', 'yellow',
+                      'orange']:
+        bet = turtle.textinput("Make your bet",
+                               "Guess wich turtle will win the race: ")
 
-while start_race:
-    for player in turtles_in_race:
-        run = randint(0, 15)
-        player.forward(run)
-        if player.xcor() >= f_line_pos:
-            winner = player.fillcolor()
-            start_race = False
+    start_race = False
+    winner = ""
+    if bet:
+        start_race = True
 
-runners.write(" ")
-runners.goto(-100, 0)
-if winner == bet:
-    runners.write(f"You Won! \nThe winner is {winner.upper()}",
-                  align='center',  font=('verdana', 25, 'normal'))
-else:
-    runners.write(f"Sorry, you lose. \nThe winner is {winner.upper()}",
-                  align='center',  font=('verdana', 25, 'normal'))
+    while start_race:
+        for player in turtles_in_race:
+            run = randint(0, 15)
+            player.forward(run)
+            if player.xcor() >= f_line_pos:
+                winner = player.fillcolor()
+                start_race = False
+
+    runners.write(" ")
+    runners.goto(-100, 0)
+    if winner == bet:
+        runners.write(f"You Won! \nThe winner is {winner.upper()}",
+                      align='center', font=('verdana', 25, 'normal'))
+    else:
+        runners.write(f"Sorry, you lose. \nThe winner is {winner.upper()}",
+                      align='center', font=('verdana', 25, 'normal'))
+
 
 screen.exitonclick()
